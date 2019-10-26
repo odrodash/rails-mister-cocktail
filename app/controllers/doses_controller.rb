dc8fd60537b1a3f149e4196a8b133fd23f46d473
Oscar Rodas <oscar.d.rodas@gmail.com>
2019-10-25 22:20:58 -0300

    heroku test1

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -7,7 +7,7 @@ class DosesController < ApplicationController
 
   def create
     @cocktail = Cocktail.find(params["cocktail_id"])
-    @dose = Dose.new(restaurant_params)
+    @dose = Dose.new(doses_params)
     @dose.cocktail = @cocktail
     if @dose.save
       redirect_to cocktail_path(params["cocktail_id"])
@@ -24,7 +24,7 @@ class DosesController < ApplicationController
 
   private
 
-  def restaurant_params
+  def doses_params
     params.require(:dose).permit(:description, :ingredient_id)
   end
 end
--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -2,4 +2,10 @@ class IngredientsController < ApplicationController
   def index
     @ingredients = Ingredient.all
   end
+
+  def new
+  end
+
+  def create
+  end
 end
